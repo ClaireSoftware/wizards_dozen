da1b778054cae52ebcb6d6ed7d106a613fa87a0f
Claire Chambers <cchamb@g.clemson.edu>
2018-10-13 11:23:48 -0400

    integrated main menu

--- wizards_dozen.py
+++ wizards_dozen.py
@@ -20,6 +20,9 @@ http://programarcadegames.com/python_examples/sprite_sheets/
 """
  
 import pygame
+from pygame.locals import *
+
+import os
  
 # Global constants
  
@@ -29,12 +32,92 @@ WHITE = (255, 255, 255)
 GREEN = (0, 255, 0)
 RED = (255, 0, 0)
 BLUE = (0, 0, 255)
+white=(255, 255, 255)
+black=(0, 0, 0)
+gray=(50, 50, 50)
+red=(255, 0, 0)
+green=(0, 255, 0)
+blue=(0, 0, 255)
+yellow=(255, 255, 0)
  
 # Screen dimensions
 SCREEN_WIDTH = 800
 SCREEN_HEIGHT = 600
  
+
+
+# Game Initialization
+pygame.init()
  
+# Center the Game Application
+os.environ['SDL_VIDEO_CENTERED'] = '1'
+ 
+ 
+# Text Renderer
+def text_format(message, textFont, textSize, textColor):
+    newFont=pygame.font.Font(textFont, textSize)
+    newText=newFont.render(message, 0, textColor)
+ 
+    return newText
+ 
+# Game Fonts
+font = "Retro.ttf"
+ 
+ 
+# Game Framerate
+clock = pygame.time.Clock()
+FPS=30
+
+def main_menu(screen):
+    screen_width=800
+    screen_height=600
+    screen.fill(blue);
+ 
+    menu=True
+    selected="start"
+ 
+    while menu:
+        for event in pygame.event.get():
+            if event.type==pygame.QUIT:
+                pygame.quit()
+                quit()
+            if event.type==pygame.KEYDOWN:
+                if event.key==pygame.K_UP:
+                    selected="start"
+                elif event.key==pygame.K_DOWN:
+                    selected="quit"
+                if event.key==pygame.K_RETURN:
+                    if selected=="start":
+                        return 0;
+                    if selected=="quit":
+                        pygame.quit()
+                        quit()
+ 
+        # Main Menu UI
+        title=text_format("Wizards Dozen", font, 90, yellow)
+        if selected=="start":
+            text_start=text_format("START", font, 75, white)
+        else:
+            text_start = text_format("START", font, 75, black)
+        if selected=="quit":
+            text_quit=text_format("QUIT", font, 75, white)
+        else:
+            text_quit = text_format("QUIT", font, 75, black)
+ 
+        title_rect=title.get_rect()
+        start_rect=text_start.get_rect()
+        quit_rect=text_quit.get_rect()
+ 
+        # Main Menu Text
+        screen.blit(title, (screen_width/2 - (title_rect[2]/2), 80))
+        screen.blit(text_start, (screen_width/2 - (start_rect[2]/2), 300))
+        screen.blit(text_quit, (screen_width/2 - (quit_rect[2]/2), 360))
+        pygame.display.update()
+        clock.tick(FPS)
+        pygame.display.set_caption("Python - Pygame Simple Main Menu Selection")
+
+
+
 class Player(pygame.sprite.Sprite):
     """
     This class represents the bar at the bottom that the player controls.
@@ -263,7 +346,7 @@ def main():
     screen = pygame.display.set_mode(size)
  
     pygame.display.set_caption("Side-scrolling Platformer")
- 
+    main_menu(screen)
     # Create the player
     player = Player()
  
